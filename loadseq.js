cd1313944a4c5c4624ccd515a01a6ba55d3bd5ab
Gabriele Motta <gabriele.motta@gmail.com>
2020-11-22 23:26:49 +0100

    first loadseq.js version

---
+++ loadseq.js
@@ -0,0 +1,44 @@
+//jload
+
+//no promises for backward compatibility
+
+console.log("loadseq ---- loading main script...");
+
+
+const loadseq = function(JSsarray) {
+	
+	script = document.createElement('script');
+	script.src = JSsarray.shift();
+	
+	if (typeof JSsarray !== 'undefined' && JSsarray.length > 0) {
+		
+		script.addEventListener('load', function() {
+
+			loadseq(JSsarray);
+		
+		});
+ 
+	}
+	else{
+		
+		script.addEventListener('load', function() {
+
+			console.log("loadseq ---- DONE");
+		
+		});		
+		
+		
+	}
+ 
+	document.head.appendChild(script);
+ 
+    };
+	
+console.log("loadseq ready");
+	
+
+        
+
+        
+
+        
